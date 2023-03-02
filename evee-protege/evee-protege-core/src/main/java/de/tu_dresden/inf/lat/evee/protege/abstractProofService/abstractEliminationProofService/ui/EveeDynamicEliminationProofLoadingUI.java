ca6ac9d193e8466e8b83d32573ba794c1eeba37c
Tom Friese <papapeace@t-online.de>
2023-03-02 09:26:50 +0100

    plugin: bugfix

--- evee-protege/evee-protege-core/src/main/java/de/tu_dresden/inf/lat/evee/protege/abstractProofService/abstractEliminationProofService/ui/EveeDynamicEliminationProofLoadingUI.java
+++ evee-protege/evee-protege-core/src/main/java/de/tu_dresden/inf/lat/evee/protege/abstractProofService/abstractEliminationProofService/ui/EveeDynamicEliminationProofLoadingUI.java
@@ -3,6 +3,7 @@ package de.tu_dresden.inf.lat.evee.protege.abstractProofService.abstractEliminat
 import de.tu_dresden.inf.lat.evee.protege.abstractProofService.AbstractEveeDynamicProofAdapter;
 import de.tu_dresden.inf.lat.evee.protege.abstractProofService.ui.EveeDynamicProofLoadingUI;
 import de.tu_dresden.inf.lat.evee.protege.abstractProofService.abstractEliminationProofService.preferences.AbstractEveeEliminationProofPreferencesManager;
+import org.protege.editor.core.ProtegeManager;
 import org.protege.editor.owl.OWLEditorKit;
 
 import javax.swing.*;
@@ -24,8 +25,9 @@ public class EveeDynamicEliminationProofLoadingUI extends EveeDynamicProofLoadin
             return;
         }
         SwingUtilities.invokeLater(() -> {
-            JFrame subOptimalProofMessageFrame = new JFrame("Proof generation cancelled");
-            subOptimalProofMessageFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            JDialog subOptimalProofMessageDialog = new JDialog(ProtegeManager.getInstance().getFrame(this.editorKit.getWorkspace()));
+            subOptimalProofMessageDialog.setTitle("Proof generation cancelled");
+            subOptimalProofMessageDialog.setModalityType(Dialog.ModalityType.DOCUMENT_MODAL);
             JPanel subOptimalProofMessagePanel = new JPanel(new GridLayout(3, 1, 5, 5));
             JLabel subOptimalProofMessageLabel = new JLabel(
                     "Due to cancellation of the proof generation, you might be seeing a sub-optimal proof",
@@ -35,16 +37,17 @@ public class EveeDynamicEliminationProofLoadingUI extends EveeDynamicProofLoadin
             JCheckBox subOptimalProofMessageCheckBox = new JCheckBox("Don't show this message again", false);
             subOptimalProofMessageCheckBox.addItemListener(this);
             JButton subOptimalProofMessageButton = new JButton("OK");
-            subOptimalProofMessageButton.addActionListener(e -> subOptimalProofMessageFrame.dispose());
+            subOptimalProofMessageButton.addActionListener(e -> subOptimalProofMessageDialog.dispose());
             subOptimalProofMessageCheckBox.setHorizontalAlignment(JCheckBox.CENTER);
             subOptimalProofMessagePanel.add(subOptimalProofMessageLabel);
             subOptimalProofMessagePanel.add(subOptimalProofMessageCheckBox);
             subOptimalProofMessagePanel.add(subOptimalProofMessageButton);
-            subOptimalProofMessageFrame.getContentPane().add(subOptimalProofMessagePanel);
-            subOptimalProofMessageFrame.pack();
-            subOptimalProofMessageFrame.setSize(600, 150);
-            subOptimalProofMessageFrame.setLocationRelativeTo(SwingUtilities.getWindowAncestor(this.editorKit.getOWLWorkspace()));
-            subOptimalProofMessageFrame.setVisible(true);
+            subOptimalProofMessageDialog.getContentPane().add(subOptimalProofMessagePanel);
+            subOptimalProofMessageDialog.pack();
+            subOptimalProofMessageDialog.setSize(600, 150);
+            subOptimalProofMessageDialog.setLocationRelativeTo(SwingUtilities.getWindowAncestor(
+                    ProtegeManager.getInstance().getFrame(this.editorKit.getWorkspace())));
+            subOptimalProofMessageDialog.setVisible(true);
         });
     }
 
