1ca2548408fe6b12684e040a91e97f7b9413ffc3
Patrick Koopmann <patrick.koopmann@tu-dresden.de>
2023-03-14 16:32:48 +0100

    justifications

--- evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/proofs/tools/JustificationGenerator.java
+++ evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/proofs/tools/JustificationGenerator.java
@@ -39,33 +39,60 @@ public class JustificationGenerator<SENTENCE,ONTOLOGY> {
 
     public Collection<Set<SENTENCE>> getJustifications(SENTENCE sentence) throws ProofGenerationException {
         IProof<SENTENCE> proof = proofGenerator.getProof(sentence);
+
+//        System.out.println("Proof: ");
+//        System.out.println(proof);
+
         justificationBuffer.clear();
+
         return getJustifications(proof, proof.getFinalConclusion(), new HashSet<>());
     }
 
     private Map<SENTENCE, Set<Set<SENTENCE>>> justificationBuffer = new HashMap<>();
 
+    /**
+     * Problem with this implementation: if the derivation structure contains a lot of cycles,
+     * the caching mechanism will not fire and may lead to an exponential execution tree
+     */
     private Set<Set<SENTENCE>> getJustifications(IProof<SENTENCE> proof, SENTENCE sentence, Set<SENTENCE> branch) {
         if(justificationBuffer.containsKey(sentence)){
+//            System.out.println("justification already known for "+sentence);
             return justificationBuffer.get(sentence);
-        } else if(branch.contains(sentence))
+        } else if(branch.contains(sentence)) {
+//            System.out.println("Cycle in branch - returning no justification for "+sentence);
             return Collections.emptySet();
+        }
         else {
             Set<Set<SENTENCE>> justifications = new HashSet<>();
+            boolean cycle = false;
             for(IInference<SENTENCE> inference: proof.getInferences(sentence)){
-                if (ProofTools.isAsserted(inference))
+//                System.out.println("Justifications based on ");
+                System.out.println(inference);
+                if (ProofTools.isAsserted(inference)) {
                     justifications.add(Collections.singleton(sentence));
+//                    System.out.println("For "+sentence);
+//                    System.out.println("A singleton");
+                }
                 else {
                     List<Set<Set<SENTENCE>>> premiseJustifications = new LinkedList<>();
                     Set<SENTENCE> branch2 = new HashSet<>(branch);
                     branch2.add(sentence);
                     inference.getPremises()
                             .forEach(s -> premiseJustifications.add(getJustifications(proof, s, branch2)));
-                    justifications.addAll(GeneralTools.unions(premiseJustifications));
+//                    System.out.println("For " + sentence);
+                    if(premiseJustifications.contains(Collections.emptySet())){
+//                        System.out.println("cycle found - not caching since justification may be incomplete");
+                        cycle=true;
+                    } else {
+//                        System.out.println("Union over " + premiseJustifications);
+                        justifications.addAll(GeneralTools.unions(premiseJustifications));
+//                        System.out.println("Which is " + GeneralTools.unions(premiseJustifications));
+                    }
                 }
             }
-            System.out.println("Putting as justifications: "+justifications);
-            justificationBuffer.put(sentence, justifications);
+//            System.out.println("Putting as justification for "+sentence+": "+justifications);
+            if(!cycle)
+                justificationBuffer.put(sentence, justifications);
             return justifications;
         }
     }
