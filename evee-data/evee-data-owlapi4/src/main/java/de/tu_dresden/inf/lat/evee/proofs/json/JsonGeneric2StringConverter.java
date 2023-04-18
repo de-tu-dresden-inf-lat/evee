d2bbd8546e005d1e7767b4c418f72351cdb16ca3
Christian Al-rabaa (Student of Lukas) <al-rabbaa@hotmail.com>
2023-04-18 19:52:39 +0200

    Json ExtendedAxiom

--- evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/proofs/json/JsonGeneric2StringConverter.java
+++ evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/proofs/json/JsonGeneric2StringConverter.java
@@ -1,6 +1,7 @@
 package de.tu_dresden.inf.lat.evee.proofs.json;
 
 import com.fasterxml.jackson.databind.util.StdConverter;
+import de.tu_dresden.inf.lat.evee.proofs.data.AbstractExtendedAxiom;
 import de.tu_dresden.inf.lat.prettyPrinting.formatting.Formatter;
 import de.tu_dresden.inf.lat.prettyPrinting.formatting.ParsableOWLFormatter;
 import org.semanticweb.owlapi.model.OWLAxiom;
@@ -18,7 +19,10 @@ public class JsonGeneric2StringConverter extends StdConverter<Object, String> {
                 throw new IllegalStateException("Could not format axiom: " + owlAxiom);
             }
             return ret;
-        } else
+        }else if(object instanceof AbstractExtendedAxiom){
+            return ((AbstractExtendedAxiom) object).getJSONString();
+        }
+        else
             return object.toString();
     }
 }
