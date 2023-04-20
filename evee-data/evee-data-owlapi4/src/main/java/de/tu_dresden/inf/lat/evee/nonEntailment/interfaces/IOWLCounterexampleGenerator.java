4a5971c1007ef6ba6aa7bde195872b0225ac9277
Stefan Borgwardt <stefan.borgwardt@tu-dresden.de>
2023-04-20 13:36:20 +0200

    getRoot method

--- evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/nonEntailment/interfaces/IOWLCounterexampleGenerator.java
+++ evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/nonEntailment/interfaces/IOWLCounterexampleGenerator.java
@@ -1,8 +1,8 @@
 package de.tu_dresden.inf.lat.evee.nonEntailment.interfaces;
 
-
-import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLIndividualAxiom;
 
 public interface IOWLCounterexampleGenerator extends IOWLNonEntailmentExplainer<OWLIndividualAxiom>, IOWLModelGenerator {
+    IRI getRoot();
 }
