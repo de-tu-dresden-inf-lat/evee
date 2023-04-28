0583fed8001ac6d0a0c1cdd6278d9a9d3daf26b1
MKotlov789 <mikhailits161@yandex.ru>
2023-04-28 13:01:36 +0300

    ModelGenerator plugin

--- evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/nonEntailment/interfaces/IOWLCounterexampleGenerator.java
+++ evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/nonEntailment/interfaces/IOWLCounterexampleGenerator.java
@@ -3,6 +3,8 @@ package de.tu_dresden.inf.lat.evee.nonEntailment.interfaces;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLIndividualAxiom;
 
+import java.util.Set;
+
 public interface IOWLCounterexampleGenerator extends IOWLNonEntailmentExplainer<OWLIndividualAxiom>, IOWLModelGenerator {
-    IRI getRoot();
+    Set<IRI> getMarkedIndividuals();
 }
