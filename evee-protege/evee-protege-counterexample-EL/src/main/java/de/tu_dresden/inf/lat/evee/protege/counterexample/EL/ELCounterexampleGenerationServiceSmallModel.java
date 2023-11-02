d10e21df35eaa253d634149a5ed4004acaa97d3a
MKotlov789 <mikhailits161@yandex.ru>
2023-11-02 19:15:37 +0300

    Add small model generator for OWL API 5, add tree model generator, bugfix

---
+++ evee-protege/evee-protege-counterexample-EL/src/main/java/de/tu_dresden/inf/lat/evee/protege/counterexample/EL/ELCounterexampleGenerationServiceSmallModel.java
@@ -0,0 +1,16 @@
+package de.tu_dresden.inf.lat.evee.protege.counterexample.EL;
+
+import de.tu_dresden.inf.lat.evee.protege.nonEntailment.counterexample.AbstractCounterexampleGenerationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ELCounterexampleGenerationServiceSmallModel extends AbstractCounterexampleGenerationService {
+    private final Logger logger = LoggerFactory.getLogger(ELCounterexampleGenerationServiceSmallModel.class);
+
+    public ELCounterexampleGenerationServiceSmallModel() {
+        super();
+        setCounterexampleGenerator(new ELCounterexampleGenerator(false));
+        setSupportsExplanationMessage("Please enter some observation containing a single OWLSubClassOfAxiom expressed in EL");
+    }
+
+}
