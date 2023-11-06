897a9a81ec91bf3690698edff8a8591aaa02fdd5
Tom Friese <papapeace@t-online.de>
2023-11-06 19:15:26 +0100

    plugin: counterexample: empty methods for changing signature due to interface adjustments

--- evee-protege/evee-protege-counterexample-EL/src/main/java/de/tu_dresden/inf/lat/evee/protege/counterexample/EL/ELCounterexampleGenerationServiceSmallModel.java
+++ evee-protege/evee-protege-counterexample-EL/src/main/java/de/tu_dresden/inf/lat/evee/protege/counterexample/EL/ELCounterexampleGenerationServiceSmallModel.java
@@ -1,8 +1,13 @@
 package de.tu_dresden.inf.lat.evee.protege.counterexample.EL;
 
 import de.tu_dresden.inf.lat.evee.protege.nonEntailment.counterexample.AbstractCounterexampleGenerationService;
+import de.tu_dresden.inf.lat.evee.protege.nonEntailment.interfaces.ISignatureModificationEventListener;
+import org.semanticweb.owlapi.model.OWLEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Set;
 
 public class ELCounterexampleGenerationServiceSmallModel extends AbstractCounterexampleGenerationService {
     private final Logger logger = LoggerFactory.getLogger(ELCounterexampleGenerationServiceSmallModel.class);
@@ -11,5 +16,15 @@ public class ELCounterexampleGenerationServiceSmallModel extends AbstractCounter
         super();
         setCounterexampleGenerator(new ELCounterexampleGenerator(false));
         setSupportsExplanationMessage("Please enter some observation containing a single OWLSubClassOfAxiom expressed in EL");
+    }
+
+    @Override
+    public void registerSignatureModificationEventListener(ISignatureModificationEventListener listener) {
+
+    }
+
+    @Override
+    public Set<OWLEntity> getAdditionalSignatureNames() {
+        return Collections.emptySet();
     }
 }
