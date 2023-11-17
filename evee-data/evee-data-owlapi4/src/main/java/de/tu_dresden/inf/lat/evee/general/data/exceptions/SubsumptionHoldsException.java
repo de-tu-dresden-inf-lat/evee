e0f303f329af45f450d79e4dadd0fd36258f6284
Stefan Borgwardt <stefan.borgwardt@tu-dresden.de>
2023-11-17 10:33:27 +0100

    Better error messages for counterexample services

--- evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/general/data/exceptions/SubsumptionHoldsException.java
+++ evee-data/evee-data-owlapi4/src/main/java/de/tu_dresden/inf/lat/evee/general/data/exceptions/SubsumptionHoldsException.java
@@ -2,12 +2,12 @@ package de.tu_dresden.inf.lat.evee.general.data.exceptions;
 
 public class SubsumptionHoldsException extends Exception {
     public SubsumptionHoldsException () {
-        super("Concept on the right-hand side is subsumed by the concept on the left-hand side");
+        super("Axiom is already entailed");
     }
     public SubsumptionHoldsException (String ConceptA, String ConceptB) {
-        super( ConceptA+ " is subsumed by "+ConceptB);
+        super(ConceptA + " is subsumed by " + ConceptB);
     }
     public SubsumptionHoldsException (String errorMessage) {
         super(errorMessage);
-    };
+    }
 }
